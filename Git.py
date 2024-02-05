a0358a56220e05433b902892de7c6d4d0dbe10c5
Anidanik <anidanik55@gmail.com>
2024-02-05 17:25:45 +0300

    Homework2

---
+++ Git.py
@@ -0,0 +1,17 @@
+# Состояния файлов unstaged (modified/untracked) - staged - committed
+# git status
+# git add
+# git commit (-a авто сохранение/ -m комментарий )
+# хеш - 32-х символьный произвольный класс commit
+# git restore работает как git add , удаляет файлы из unstaged стадии
+# git restore -- staged переводит из стадии staged в unstaged
+# git log
+# q выход из гит мода на винде  ctrl c -linux
+# git checkout/switch хеш коммита
+# Head - текущее местоположение
+# git branch выводит все ветки в проекте
+# git checkout -b 'ITacademy' создает ветку и переключается на неё
+# git merge и git rebase слияние веток
+# git diff сравнить изменения с прошлым коммитом
+# merge конфликты - когда при слиянии git не знает что оставить и что удалить
+# git merge --continue/ git merge -- abort первая в том случае , если все конфликты решены, при аборте мердж отменяется
