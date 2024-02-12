16fe1d2f30ea94ad3b0aa375d4a5dbed12c7cc42
Anidanik <anidanik55@gmail.com>
2024-02-12 15:23:00 +0300

    Homework3 task3

--- src/homework3/task3.py
+++ src/homework3/task3.py
@@ -4,9 +4,11 @@ def tuple_practice():
     print(cortej,spisok)
     a,b,c ='a',2,'python'
     print(a,b,c)
-    d=tuple()
-    print(len(d))
-    for i in d:
-        print(i)
+    t=(1,)
+    for i in t:
+         print(i)
+         print(i + 1)
+         print(i + 2)
+    print(len(t))
 tuple_practice()
 
