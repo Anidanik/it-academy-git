542dd551ea8c01c884f614e749eb333d45edd0b0
Anidanik <anidanik55@gmail.com>
2024-02-21 19:32:37 +0300

    Homework4 update task3 and 4

--- src/Homework4/task3.py
+++ src/Homework4/task3.py
@@ -1,8 +1,3 @@
 def identical(a,b):
-    c=0
-    for i in set(a):
-        for j in set(b):
-            if i == j:
-                c += 1
-    print(c)
-identical([1, 2, 3, 4, 5],[6, 4, 2, 1])
+    print(len(set.intersection(set(a),set(b))))
+identical([1, 2, 3, 4, 5, 8],[6, 4, 2, 1, 5])
--- src/Homework4/task4.py
+++ src/Homework4/task4.py
@@ -1,8 +1,3 @@
 def unique(a,b):
-    c = 0
-    for i in set(a):
-        for j in set(b):
-            if i == j:
-                c += 1
-    print(len(set(a+b))-c)
-unique([1, 2, 3, 4, 5, 7],[6, 4, 2, 1, 7])
+    print(len(set.symmetric_difference(set(a), set(b))))
+unique([1, 2, 3, 4, 5, 7, 8],[6, 4, 2, 1, 7, 9])
